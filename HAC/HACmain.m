8aa79033fc3d46c9936a604c3538804a0f56cb1f
Daniel J. Hanggi <djh325@cornell.edu>
2014-12-01 11:38:53 -0500

    Updated HAC

--- HAC/HACmain.m
+++ HAC/HACmain.m
@@ -1,7 +1,7 @@
 addpath jsonlab
 
 songData = SongParser.parseSongData('../DataParsing/csv/all_songs-modified.csv');
-songData = songData(1:10);
+songData = songData(1:800);
 
 dist = Song.distanceMatrix(songData);
 squareDist = squareform(dist);
@@ -9,4 +9,3 @@ squareDist = squareform(dist);
 Cluster.clusterToJSON(songData, 'single', 'all-songs-clustering-single.json', squareDist);
 Cluster.clusterToJSON(songData, 'complete', 'all-songs-clustering-complete.json', squareDist);
 Cluster.clusterToJSON(songData, 'average', 'all-songs-clustering-average.json', squareDist);
-Cluster.clusterToJSON(songData, 'centroid', 'all-songs-clustering-centroid.json', squareDist);
