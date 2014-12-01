1bb21722d4a5c71f9727f428880942ad4055c766
djh325 <djh325@cornell.edu>
2014-12-01 02:05:17 -0500

    Removed use of tables to parse .csv files, but changed all unknown fields/ambiguous commas in .csv files to backtick

--- HAC/HACmain.m
+++ HAC/HACmain.m
@@ -1,6 +1,6 @@
 addpath HAC/jsonlab
 
-songData = SongParser.parseSongData('DataParsing\csv\all_songs-modified.csv');
+songData = SongParser.parseSongData('DataParsing/csv/all_songs-modified.csv');
 
 dist = zeros(length(songData));
 for r = 1:length(songData)
@@ -11,16 +11,27 @@ for r = 1:length(songData)
 end
 
 squareDist = squareform(dist);
+
 linkSingle = linkage(squareDist, 'single');
-% linkComple = linkage(squareDist, 'complete');
-% linkAverag = linkage(squareDist, 'average');
-% linkCentro = linkage(squareDist, 'centroid');
-
 clustersSingle = Cluster.agglomerate(songData, linkSingle);
-% clustersComple = Cluster.agglomerate(songData, linkComple);
-% clustersAverag = Cluster.agglomerate(songData, linkAverag);
-% clustersCentro = Cluster.agglomerate(songData, linkCentro);
-
 fid = fopen('HAC/all-songs-clustering-single.json', 'w');
 fprintf(fid, clustersSingle(end).outputClusterJSON());
+fclose(fid);
+
+linkComple = linkage(squareDist, 'complete');
+clustersComple = Cluster.agglomerate(songData, linkComple);
+fid = fopen('HAC/all-songs-clustering-complete.json', 'w');
+fprintf(fid, clustersComple(end).outputClusterJSON());
+fclose(fid);
+
+linkAverag = linkage(squareDist, 'average');
+clustersAverag = Cluster.agglomerate(songData, linkAverag);
+fid = fopen('HAC/all-songs-clustering-average.json', 'w');
+fprintf(fid, clustersAverag(end).outputClusterJSON());
+fclose(fid);
+
+linkCentro = linkage(squareDist, 'centroid');
+clustersCentro = Cluster.agglomerate(songData, linkCentro);
+fid = fopen('HAC/all-songs-clustering-centroid.json', 'w');
+fprintf(fid, clustersCentro(end).outputClusterJSON());
 fclose(fid);
