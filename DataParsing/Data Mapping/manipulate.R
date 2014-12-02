21a683739d370392d0727a21436c2bc9e31de6aa
Veena Calambur <vc233@cornell.edu>
2014-12-02 12:03:07 -0500

    In Progress dendrogram
    So far have incorporated the dendrogram into the index page, via
    iFrame, but I ran into  bug as I added this into the index.php, but my
    text wrap functions stopped working (d3 requires special text wrap)
    
    So far upon hovering on a node, the average danceability / valence /
    energy will display for this node as well as all the song elements that
    belong to the sub cluster

--- DataParsing/Data Mapping/manipulate.R
+++ DataParsing/Data Mapping/manipulate.R
@@ -1,7 +1,7 @@
 setwd("Desktop/CS478final")
 
 tdance0_1 = read.csv("dance_0to1_data.csv", header = TRUE)
-tdance1_2 = read.csv("dance_1to2_data.csv", header = TRUE)
+tdance1_2 = read.csv("dance_1to2_data.csv")
 tdance2_3 = read.csv("dance_2to3_data.csv", header = TRUE)
 tdance3_4 = read.csv("dance_3to4_data.csv", header = TRUE)
 tdance4_5 = read.csv("dance_4to5_data.csv", header = TRUE)
@@ -22,44 +22,42 @@ nodance = merge(tdance3_4,nodance,by=names(tdance0_1), all = TRUE)
 nodance = merge(tdance4_5,nodance,by=names(tdance0_1), all = TRUE)
 
 dance = merge(yd, nodance, by = names(yd), all = TRUE)
-
-<<<<<<< Updated upstream
 attach(dance)
-plot(-3.279225e-06*valence*I(tempo^2) +  -8.160651e-04*energy*I(loudness^2) +
-=======
-#updating NA values in instrumentalness 
-for(i in 1:length(dance$speechiness)){
-  if(is.na(dance$speechiness[i])){
-    dance$speechiness[i] = 0.10720
+
+#fill in NA data with mean: 
+for(i in 1:length(speechiness)){
+  if(is.na(speechiness[i])){
+    speechiness[i] = summary(speechiness)["Mean"]
   }
 }
 
-for(i in 1:length(dance$instrumentalness)){
-  if(is.na(dance$instrumentalness[i])){
-    dance$instrumentalness[i] = 0.319900
+for(i in 1:length(instrumentalness)){
+  if(is.na(instrumentalness[i])){
+    instrumentalness[i] = summary(instrumentalness)["Mean"]
   }
 }
 
-write.csv(file="no_na_songs.csv", x=dance)
+dance$speechiness = speechiness
+dance$instrumentalness = instrumentalness
 
-plot(-3.279225e-06*valence*I(tempo^2) +  -8.160651e-04*energy*I(loudness^2) +  
->>>>>>> Stashed changes
+#find correlations among variables in the data
+correlations = cor(dance[,c("key","energy","liveness", "tempo", "speechiness", "acousticness", "instrumentalness", "mode", 
+                            "time_signature", "duration", "loudness", "valence", "danceability")])
+
+plot(-3.279225e-06*danceability*I(tempo^2) +  -8.160651e-04*energy*I(loudness^2) +  
        3.218223e-01*acousticness*I(energy^2)+-3.279225e-06*liveness*speechiness
      +3.793979e-04*tempo*energy+-7.492498e-03*loudness*acousticness
-     + 7.001209e-02*instrumentalness*valence+ -6.132415e-04*valence*loudness
-     +1.359087e-02*energy*loudness*I(valence^2), valence, pch =16)
+     + 7.001209e-02*instrumentalness*danceability+ -6.132415e-04*valence*loudness
+     +1.359087e-02*energy*loudness*I(danceability^2), valence, pch =16)
 
-plot(-3.279225e-06*valence*I(tempo^2) +  -8.160651e-04*energy*I(loudness^2) +
+plot(-3.279225e-06*valence*I(tempo^2) +  -8.160651e-04*energy*I(loudness^2) +  
        3.218223e-01*acousticness*I(energy^2)+-3.279225e-06*liveness*speechiness
      +3.793979e-04*tempo*energy+-7.492498e-03*loudness*acousticness
      + 7.001209e-02*instrumentalness*valence+ -6.132415e-04*valence*loudness
      +1.359087e-02*energy*loudness*I(valence^2), danceability, pch =16)
 
-plot((-3.126570 * 10^-1)*acousticness
-  +  (-7.503000 * 10^-4) * tempo
-     +   (1.719260 * 10^-2) * loudness
-     +  (-1.399756 * 10^-1) * valence
-     +   (2.950410 * 10^-2) * danceability
-     +  (-2.433880 * 10^-1) * speechiness
-     +   (1.868210 * 10^-2) * loudness * danceability
-     , energy, pch = 16)
+plot(-0.3126570*acousticness +   0.0007503*tempo + 0.0171926*loudness + 0.1399756*valence + -0.0295041*danceability
+      +0.2433880*speechiness +  0.0186821*loudness*danceability, 
+      energy, pch =16)
+
+
